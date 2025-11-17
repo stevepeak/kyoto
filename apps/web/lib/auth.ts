dba0c53d549c9c26136d2785fd2a317ae3e0a1a6
Steve^ <steve@stevepeak.net>
2025-11-17 16:03:59 +0700

    add email scope

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -78,6 +78,7 @@ export function getAuth() {
         github: {
           clientId: process.env.GITHUB_CLIENT_ID!,
           clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+          scope: ['email'],
         },
       },
     })
