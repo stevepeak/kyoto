2046c97299808f8c3eb40c2ebdf8e640539fd78e
Steve^ <steve@stevepeak.net>
2025-12-01 09:14:29 +0700

    remove beta banner

--- apps/web/components/layout/app-layout.tsx
+++ apps/web/components/layout/app-layout.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from 'react'
 import type { BreadcrumbItem } from '@/components/common/Breadcrumbs'
 
-import { BetaBanner } from '@/components/common/beta-banner'
 import { TopNav } from './top-nav'
 import { AppFooter } from './app-footer'
 
@@ -14,7 +13,7 @@ interface Props {
 export function AppLayout({ children, breadcrumbs, right }: Props) {
   return (
     <div className="min-h-screen w-full flex flex-col bg-background">
-      <BetaBanner />
+      {/* <BetaBanner /> */}
       <TopNav breadcrumbs={breadcrumbs} right={right} />
       <div className="flex-1">{children}</div>
       <AppFooter />
