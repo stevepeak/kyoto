4c8f4cc6a7d02e886afb961f46b5c3bc35f60761
Steve^ <steve@stevepeak.net>
2025-12-08 15:28:21 -0700

    remove old db

--- packages/db-old/src/types.ts
+++
@@ -1,3 +0,0 @@
-export * from './column-types.js'
-export * from './types.gen.js'
-export type { DB } from './types.gen.js'
--- packages/db-old/src/utils.ts
+++
@@ -1,5 +0,0 @@
-import { type RawBuilder, sql } from 'kysely'
-
-export function json<T>(value: T): RawBuilder<T> {
-  return sql`CAST(${JSON.stringify(value)} AS JSONB)`
-}
